2513c43ca8f66a80959dbddddc903d8be88b92e4
dowar <gonzalo@dowar.xyz>
2023-09-21 13:49:57 -0600

    template user remove

---
+++ resources/views/mails/userRemove.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
+    xmlns:o="urn:schemas-microsoft-com:office:office">
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <h1 style="display: flex;justify-content: center;"><b><u>
+                Agradecimiento a los Socios que se retiran o se desafilian
+            </u></b>
+    </h1>
+
+    <p>
+        Gracias señor {{ $nombre }}, por haber sido parte de la Asociación
+        de Militares Nueva Visión F.A. <b><u>
+                A partir de este momento ya no perteneces a nuestra Asociación .
+            </u></b>.
+    </p>
+
+</body>
+
+</html>
