f67bf6402adee0fe37d25c8c52074ed39eed7870
Dowar <gonzalo@dowar.xyz>
2023-10-05 16:33:54 -0600

    minor fix(10)

--- app/Http/Controllers/ProcesoSocioController.php
+++ app/Http/Controllers/ProcesoSocioController.php
@@ -22,7 +22,7 @@ class ProcesoSocioController extends Controller
   public function store(Request $request)
   {
     $socio = Socio::find($request->socio);
-    if ($socio->abono < 3) {
+    if ($socio->pago == 2) {
       $proceso = new ProcesoSocio();
       $proceso->socio_id = $request->socio;
       $proceso->lugar = $request->lugar;
