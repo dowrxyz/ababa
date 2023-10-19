dfdb86765bd38e8eb962f1b066d24484b9466b16
Dowar <gonzalo@dowar.xyz>
2023-10-19 10:13:27 -0600

    code format

--- app/Http/Controllers/AporteController.php
+++ app/Http/Controllers/AporteController.php
@@ -8,45 +8,40 @@ use App\Models\Aporte;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Request;
 
-
 class AporteController extends Controller
 {
-
-    public function index()
-    {
-        if(!isset(Auth::user()->name))
-        {
-            return redirect()->route('login');
-        }
-
-        return view('aportes');
+  public function index()
+  {
+    if (!isset(Auth::user()->name)) {
+      return redirect()->route("login");
     }
 
+    return view("aportes");
+  }
 
-    public function store(Request $request)
-    {
-        $aporte = new Aporte();
-        $aporte->id_usuario = $request->usuario_id;
-        $aporte->ano = $request->ano;
-        $aporte->tipo_aporte = $request->tipo_aporte;
-        $aporte->aporte = $request->aporte;
-        $aporte->save();
+  public function store(Request $request)
+  {
+    $aporte = new Aporte();
+    $aporte->id_usuario = $request->usuario_id;
+    $aporte->ano = $request->ano;
+    $aporte->tipo_aporte = $request->tipo_aporte;
+    $aporte->aporte = $request->aporte;
+    $aporte->save();
 
-        return $aporte;
-    }
+    return $aporte;
+  }
 
-    public function show()
-    {
-        return Aporte::all();
-    }
+  public function show()
+  {
+    return Aporte::all();
+  }
 
-    public function update(Request $request)
-    {
-        $aporte = Aporte::find($request->id);
-        $aporte->ano = $request->ano;
-        $aporte->tipo_aporte = $request->tipo_aporte;
-        $aporte->aporte = $request->aporte;
-        $aporte->save();
-    }
-
+  public function update(Request $request)
+  {
+    $aporte = Aporte::find($request->id);
+    $aporte->ano = $request->ano;
+    $aporte->tipo_aporte = $request->tipo_aporte;
+    $aporte->aporte = $request->aporte;
+    $aporte->save();
+  }
 }
